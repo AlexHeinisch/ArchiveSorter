2df982e2504a1cfb9d3ce6ce9fad2e869b140df6
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2023-12-21 12:55:53 +0100

    feat: file loading with hashing and date extraction is possible

--- archivesorter/app.py
+++ archivesorter/app.py
@@ -1,6 +1,12 @@
+from sqlmodel import Session
 from typer import Typer
+from archivesorter.file_manager import (
+    extract_file_info_by_path,
+    get_all_file_paths_by_root_folder,
+)
 from config import settings
-from database.db import initialize_db
+from database.db import initialize_db, engine
+from rich.progress import track
 
 
 app = Typer(name=settings.app_name)
@@ -10,6 +16,11 @@ initialize_db()
 @app.command()
 def load(input_folder_path: str):
     print('Start loading files...')
+    with Session(engine) as session:
+        for file in track(list(get_all_file_paths_by_root_folder(input_folder_path))):
+            file_info = extract_file_info_by_path(file)
+            session.add(file_info)
+            session.commit()
 
 
 @app.command()
--- archivesorter/file_manager.py
+++ archivesorter/file_manager.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Generator
 import os
 from archivesorter.database.models import FileInfo
+import hashlib
 
 
 def get_all_file_paths_by_root_folder(
@@ -10,12 +11,32 @@ def get_all_file_paths_by_root_folder(
     extensions = '*'
     for root, _, files in os.walk(root_folder_path):
         for file in files:
-            if file.endswith(extensions):
+            if extensions == '*' or file.endswith(extensions):
                 yield os.path.join(root, file)
 
 
 def extract_file_info_by_path(file_path: str) -> FileInfo:
-    ...
+    creation_date = datetime.fromtimestamp(os.path.getctime(file_path))
+    modified_date = datetime.fromtimestamp(os.path.getmtime(file_path))
+    file_content_hash = hash_file_content(file_path)
+    return FileInfo(
+        source_path=file_path,
+        file_last_modified=modified_date,
+        file_created=creation_date,
+        file_hash=file_content_hash,
+        photo_created=None,
+        evaluated_datetime=None,
+        evaluated_category=None,
+        computed_target_path=None,
+    )
+
+
+def hash_file_content(file_path: str) -> str:
+    sha256_hash = hashlib.sha3_256()
+    with open(file_path, 'rb') as file:
+        for chunk in iter(lambda: file.read(4096), b''):
+            sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
 
 
 def extract_image_creation_date_from_metadata(file_path: str) -> datetime:
