992c6ba812f34440252d2440b11d548a49b61d29
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2023-12-21 13:58:51 +0100

    fix: replace != with is not

--- archivesorter/info_app.py
+++ archivesorter/info_app.py
@@ -7,7 +7,31 @@ app = typer.Typer()
 
 
 @app.command()
-def num_files():
+def count():
     with Session(engine) as session:
         cnt = session.exec(select(func.count(FileInfo.id))).one()
     print('Number of loaded files: ', cnt)
+
+
+@app.command()
+def count_exif():
+    with Session(engine) as session:
+        cnt = session.exec(
+            select(func.count(FileInfo.photo_created)).where(
+                FileInfo.photo_created is not None
+            )
+        ).one()
+    print('Number of files with EXIF information: ', cnt)
+
+
+@app.command()
+def count_duplicates():
+    with Session(engine) as session:
+        base_query = select(func.count(FileInfo.id).label('hash_count')).group_by(
+            FileInfo.file_hash
+        )
+        query = select(func.count(base_query.as_scalar())).where(
+            base_query.c.hash_count > 1
+        )
+        cnt = session.exec(query).one()
+    print('Number of duplicate files: ', cnt)
