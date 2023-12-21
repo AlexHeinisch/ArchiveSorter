8fda0f30202a7d53f1541270c2d690d6d8e01c10
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2023-12-21 12:02:34 +0100

    fix: database models not being created in sqlite db

--- archivesorter/config.py
+++ archivesorter/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     app_name: str = 'PhotoArchiveSorter'
 
-    database_connection_string: str = 'sqlite://'
+    database_connection_string: str = 'sqlite:///database.db'
     database_echo: bool = True
 
 settings = Config()
--- archivesorter/database/db.py
+++ archivesorter/database/db.py
@@ -4,4 +4,5 @@ from archivesorter.config import settings
 engine = create_engine(url=settings.database_connection_string, echo=settings.database_echo)
 
 def initialize_db():
+    import archivesorter.database.models
     SQLModel.metadata.create_all(engine)
